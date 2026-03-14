5b34f4e8529e9545e9f77bde74be91bf6bf12f88
Gabor Kotel <gabor.kotel@gmail.com>
2026-03-14 23:47:25 +0100

    ci: change to minimal test

--- srv/spacefarer/spacefarer.service.ts
+++ srv/spacefarer/spacefarer.service.ts
@@ -1,13 +1,1 @@
-const cds = require('@sap/cds');
-
-module.exports = cds.service.impl(function () {
-  this.on('READ', 'Spacefarers', async (req) => {
-    console.info('[service] custom READ Spacefarers handler - returning static smoke data');
-    return [
-      {
-        ID: 's1a2b3c4-d5e6-f7g8-h9i0-j1k2l3m4n5o6',
-        name: 'Alara Voss',
-      },
-    ];
-  });
-});
+// No custom handlers - rely on default CDS CRUD and persistence
