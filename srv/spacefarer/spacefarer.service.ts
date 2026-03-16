954caeedfdfc93cd3a5dc83ff52e340e3ed94ed4
Gabor Kotel <gabor.kotel@gmail.com>
2026-03-16 04:46:53 +0100

    feat: add unit test for event handler

--- srv/spacefarer/spacefarer.service.ts
+++ srv/spacefarer/spacefarer.service.ts
@@ -1,1 +1,9 @@
-// No custom handlers - rely on default CDS CRUD and persistence
+/**
+ * NOT IMPLEMENTED: This function would validate and enhance the spacefarer's profile, but it's not implemented yet.
+ */
+
+function validateAndEnhanceSpacefarer() {}
+/**
+ * NOT IMPLEMENTED: This function would send a welcome email to new spacefarers, but it's not implemented yet.
+ */
+function sendCosmicWelcomeEmail() {}
